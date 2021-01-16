13f15d6b2a4ef7171fd3868a9e2205a55a992157
paparazzit <paparazzit@fgmail.com>
2021-01-16 13:52:30 +0100

    Helca initial

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+	$(".owl-carousel").owlCarousel({
+		loop: true,
+		margin: 10,
+		nav: false,
+
+		// autoWidth: true,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			600: {
+				items: 1,
+			},
+			1000: {
+				items: 1,
+			},
+		},
+	});
+});
+
+var owl = $(".owl-carousel");
+// owl.owlCarousel();
+// Go to the next item
+$(".customNextBtn").click(function () {
+	owl.trigger("next.owl.carousel");
+});
+// Go to the previous item
+$(".customPrevBtn").click(function () {
+	// With optional speed parameter
+	// Parameters has to be in square bracket '[]'
+	owl.trigger("prev.owl.carousel", [300]);
+});
